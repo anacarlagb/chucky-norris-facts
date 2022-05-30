402dba15af7dc073de21db530bf6f4d9dd5a18b3
anacarlagb <ana.carlagb@gmail.com>
2022-05-30 09:11:58 -0300

    Adding get request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/ChuckNorris'
 
 function App() {
   return (
@@ -9,14 +10,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+              <h3 className="p-3 text-center">ChuckNorris API</h3>
+              <ChuckNorris />
+          </div>
       </header>
     </div>
   );
