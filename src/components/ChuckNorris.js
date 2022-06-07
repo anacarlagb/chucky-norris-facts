bf7b7f22a5935fd14737dc938b2964e3670edb9d
anacarlagb <ana.carlagb@gmail.com>
2022-06-07 10:58:53 -0300

    Fixing Busca Aleatoria button

--- src/components/ChuckNorris.js
+++ src/components/ChuckNorris.js
@@ -5,11 +5,13 @@ import '../App.css';
 
 
 
+
 export class ChuckNorris extends React.Component {
 
    
     state = {
         category: "", 
+        aleat: "",
         data: {
             created_at: "",
             categories: [],
@@ -27,6 +29,8 @@ export class ChuckNorris extends React.Component {
 
         this.getData = this.getData.bind(this);
         this.setCategory = this.setCategory.bind(this);
+        this.setAleat = this.setAleat.bind(this);
+        this.callAleat = this.callAleat.bind(this);
 
     }
 
@@ -35,6 +39,12 @@ export class ChuckNorris extends React.Component {
           category: value,
         });
       }
+
+    setAleat(value) {
+        this.setState({
+            aleat: value,
+        });
+    }
 
     async getSearch(value) {
         // Simple GET request using fetch
@@ -48,7 +58,13 @@ export class ChuckNorris extends React.Component {
        else{
           this.setCategory(data.value);
        }
-       
+    }
+
+    async getAleat() {
+        // Simple GET request using fetch
+       const response = await fetch("https://api.chucknorris.io/jokes/random");
+       const data = await response.json();
+       this.setAleat(data.value);
     }
 
     getData(dataAPI) {
@@ -57,19 +73,42 @@ export class ChuckNorris extends React.Component {
              data: dataAPI
         });
 
+       
+      
+
     }
+
+    callAleat() {
+        this.getAleat();
+    }
+      // Usage
+     
 
     render() {
     
         return (
-            <div className="card text-center m-3">
-                <img src={logo}  width="500" height="300" />
-               
-                <div id="divBusca" >
-                    <input type="text" id="txtBusca" placeholder="Buscar por categoria.."
-                    onChange={(e) => this.getSearch(e.target.value)} />      
-                      {this.state.category}  
+
+            <div>
+                <div className="card text-center m-3" >
+                    <img src={logo}  width="500" height="300" />
+
+                
                 </div>
+
+                <div id="divBusca" style={{ position: "fixed" }}>
+                    <input type="text" id="txtBusca" placeholder="Buscar por categoria.."
+                        onChange={(e) => this.getSearch(e.target.value)} />      
+          
+                    </div>
+               <div style={{top: "91%", left: "20%", position: "fixed" }}>{this.state.category}</div> 
+
+                <div className="flex" style={{ top: "75%", left: "73%", position: "fixed" }}>
+                    <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+                        onClick={this.callAleat}>
+                        Busca Aleatória
+                    </button>
+                </div>
+                <div style={{ top: "91%", left: "20%", position: "fixed" }}>{this.state.aleat}</div> 
 
             </div>
         );
